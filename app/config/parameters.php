1e609af5f6477ff1a05fb74d68ce8339065913e4
Yurii07 <skyey07@gmail.com>
2018-09-26 16:18:14 +0300

    dev init

---
+++ app/config/parameters.php
@@ -0,0 +1,26 @@
+<?php return array (
+  'parameters' =>
+
+
+  array (
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'prestashopnoski',
+    'database_user' => 'u_prestashT',
+    'database_password' => 'UUTu3GNz',
+    'database_prefix' => 'os_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => 'localhost',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'zv1997GhjixOanMRw2UYYiEsAdI7nKa41GuvgkYkI9KBTQC5JUqf3Fi5',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-08-26',
+    'locale' => 'ru-RU',
+    'cookie_key' => 'cs5KLUO6i1GJcgwDMtavRMcUXVyr2VgkvAXFECrz5x2V5kkMCJMDMJz4',
+    'cookie_iv' => 'NbRpa8Ry',
+    'new_cookie_key' => 'def000006adaca9e1e4c9c732856b9b55f569d1966029a45d291ddf5a4157192ebf20b86b3110c27bc580fd4e48c8a49bfc953ed5c867d30068064718fb7cd1eb94045ee',
+  ),
+);
